216e78fe724f0bfc7c372bcb4f5e4549d6414904
AlexDevFr <contact@alexdev.fr>
2023-12-20 13:19:58 +0100

    First push timer.sh

---
+++ timer.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+################################################################################
+#                  ___    __          ____            ______                   #
+#                 /   |  / /__  _  __/ __ \___ _   __/ ____/____               #
+#                / /| | / / _ \| |/_/ / / / _ \ | / / /_  / ___/               #
+#               / ___ |/ /  __/>  </ /_/ /  __/ |/ / __/ / /                   #
+#              /_/  |_/_/\___/_/|_/_____/\___/|___/_/   /_/                    #
+#                                                                              #
+#                                                                              #
+# 0x16 Script Servers - Install clean server & more :)                         #
+#                                                                              #
+# FILE ->                                                                      #
+# timer.sh                                                                     #
+#                                                                              #
+#                                                                              #
+# DESCRIPTION                                                                  #
+# Simple timer for Linux                                                       #
+#                                                                              #
+# COPYRIGHT                                                                    #
+#                                                                              #
+# AUTHOR                                                                       #
+# AlexDevFr - https://wwww.alexdev.fr                                          #
+#                                                                              #
+################################################################################
+
+
+echo '\033[32m'
+	cat <<- "EOF"
+    ___    __          ____            ______    
+   /   |  / /__  _  __/ __ \___ _   __/ ____/____
+  / /| | / / _ \| |/_/ / / / _ \ | / / /_  / ___/
+ / ___ |/ /  __/>  </ /_/ /  __/ |/ / __/ / /    
+/_/  |_/_/\___/_/|_/_____/\___/|___/_/   /_/     
+                                                 
+EOF
+
+GREEN="\e[32m"
+ENDCOLOR="\e[0m"
+
+echo "${ENDCOLOR}Indiquer le temps du timer en minute :"
+read time
+
+clear
+
+echo "${GREEN}Le timer a ete programmer pour "$time "minutes.${ENDCOLOR}"
+sudo shutdown -P +$time
